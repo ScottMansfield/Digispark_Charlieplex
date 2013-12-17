a782b9e9481e550ba34d2bfeb754df6feac198be
ScottMansfield <sgmansf@gmail.com>
2013-12-17 07:30:50 -0800

    Support for Digispark shield for DigiX by adding pin mapping headers

---
+++ Digix_Pins.h
@@ -0,0 +1,9 @@
+/**
+ * Defines the pin mappings for the digispark
+ */
+
+#define CH_0 8
+#define CH_1 9
+#define CH_2 71
+#define CH_3 10
+#define CH_4 11
--- LetterDisplay.h
+++ LetterDisplay.h
@@ -4,17 +4,17 @@
 
 #include <Arduino.h>
 #include "Digispark_Charlieplex.h"
+#include "Digispark_Pins.h"
 
 #define LED_DELAY 1
 
 void LEDon(int vin, int gnd)
 {
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
+  pinMode(CH_0, INPUT);
+  pinMode(CH_1, INPUT);
+  pinMode(CH_2, INPUT);
+  pinMode(CH_3, INPUT);
+  pinMode(CH_4, INPUT);
   
   pinMode(vin, OUTPUT);
   pinMode(gnd, OUTPUT);
@@ -27,412 +27,412 @@ void LEDon(int vin, int gnd)
 
 void allOn()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 2);
-  LEDon(1, 3);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 1);
-  LEDon(3, 2);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_2);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_1);
+  LEDon(CH_3, CH_2);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showA()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showB()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showC()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showD()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2); 
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2); 
 }
 
 void showE()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showF()
 {
-  LEDon(0, 1);
-  LEDon(1, 0);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showG()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 3);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showH()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showI()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(0, 4);
-  LEDon(1, 2);
-  LEDon(1, 3);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 1);
-  LEDon(3, 2);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_2);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_1);
+  LEDon(CH_3, CH_2);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showJ()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 4);
-  LEDon(3, 4);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_3);
 }
 
 void showK()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 3);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(3, 0);
-  LEDon(3, 2);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_2);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showL()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(2, 0);
-  LEDon(3, 0);
-  LEDon(4, 0);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_4, CH_0);
 }
 
 void showM()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 1);
-  LEDon(3, 2);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_1);
+  LEDon(CH_3, CH_2);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showN()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 3);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 3);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 1);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_1);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showO()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showP()
 {
-  LEDon(0, 1);
-  LEDon(1, 0);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showQ()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 3);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showR()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 3);
-  LEDon(2, 0);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
 }
 
 void showS()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 4);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showT()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 2);
-  LEDon(1, 3);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 1);
-  LEDon(3, 2);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_2);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_1);
+  LEDon(CH_3, CH_2);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
 
 void showU()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showV()
 {
-  LEDon(0, 2);
-  LEDon(1, 0);
-  LEDon(1, 3);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showW()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 2);
-  LEDon(1, 3);
-  LEDon(1, 4);
-  LEDon(2, 0);
-  LEDon(2, 4);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_2);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_0);
+  LEDon(CH_2, CH_4);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showX()
 {
-  LEDon(0, 1);
-  LEDon(0, 4);
-  LEDon(1, 0);
-  LEDon(1, 4);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_0);
+  LEDon(CH_1, CH_4);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showY()
 {
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(1, 2);
-  LEDon(1, 3);
-  LEDon(2, 1);
-  LEDon(2, 3);
-  LEDon(3, 0);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_1, CH_2);
+  LEDon(CH_1, CH_3);
+  LEDon(CH_2, CH_1);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_0);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_3);
 }
 
 void showZ()
 {
-  LEDon(0, 1);
-  LEDon(0, 2);
-  LEDon(0, 3);
-  LEDon(0, 4);
-  LEDon(1, 2);
-  LEDon(2, 3);
-  LEDon(3, 4);
-  LEDon(4, 0);
-  LEDon(4, 1);
-  LEDon(4, 2);
-  LEDon(4, 3);
+  LEDon(CH_0, CH_1);
+  LEDon(CH_0, CH_2);
+  LEDon(CH_0, CH_3);
+  LEDon(CH_0, CH_4);
+  LEDon(CH_1, CH_2);
+  LEDon(CH_2, CH_3);
+  LEDon(CH_3, CH_4);
+  LEDon(CH_4, CH_0);
+  LEDon(CH_4, CH_1);
+  LEDon(CH_4, CH_2);
+  LEDon(CH_4, CH_3);
 }
